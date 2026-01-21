5e259d31054aaa224a8dec7c16c3b10f2d8e9fe4
gdalevicholga-ux <gdalevicholga@gmail.com>
2026-01-21 17:42:35 +0200

    LoginPositTest

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public abstract class BasePage {
+    static WebDriver driver;
+
+    public static void setDriver(WebDriver wd) {
+        driver = wd;
+    }
+
+    public void pause(int time) {
+        try {
+            Thread.sleep(time * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isElementDisplayed(WebElement element){
+        return element.isDisplayed();
+    }
+}
---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,24 @@
+package ui_tests;
+
+import dto.User;
+import manager.ApplicationManager;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+    @Test
+    public void loginPositiveTest(){
+        User user = User.builder()
+                .email("bolik@gmail.com")
+                .password("Bolik1245!")
+                .build();
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm(user);
+        loginPage.clickBtnYalla();
+        Assert.assertTrue(loginPage.isLoggedInDisplayed());
+    }
+}
